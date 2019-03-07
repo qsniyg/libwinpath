4e0bb21c703dc3b9ecb8e0e6e8d2b90d509bb6c8
qsniyg <qsniyg@mail.com>
2019-03-07 00:24:57 -0800

    Add open wrapper

--- include/libwinpath.h
+++ include/libwinpath.h
@@ -6,6 +6,10 @@
 extern "C" {
 #endif
 
+#if defined (LIBWINPATH_ENABLE_WRAPPER) && (!defined(_WIN32) || defined(LIBWINPATH_FORCE_WINDOWS))
+#define _LIBWINPATH_ENABLE_WRAPPER
+#endif
+
 #define LIBWINPATH_FILE_OPEN 1
 #define LIBWINPATH_FILE_CREATE 2
 #define LIBWINPATH_FILE_OVERWRITE 4
@@ -22,8 +26,9 @@ int libwinpath_getpath(char** dst, const char* path, int disposition);
 /* fopen wrapper */
 FILE* libwinpath_fopen(const char* filename, const char* mode);
 
-#ifdef LIBWINPATH_ENABLE_WRAPPER
+#ifdef _LIBWINPATH_ENABLE_WRAPPER
 #define fopen(...) libwinpath_fopen(__VA_ARGS__)
+#define open(...) libwinpath_open(__VA_ARGS__)
 #endif
 
 #ifdef __cplusplus
--- src/wrapper.c
+++ src/wrapper.c
@@ -1,8 +1,31 @@
 #include "libwinpath.h"
 #include <stdio.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 char libwinpath_disable_wrapper = 0;
+
+int libwinpath_open(const char *pathname, int flags, mode_t mode) {
+  if (libwinpath_disable_wrapper)
+    return open(pathname, flags, mode);
+
+  char* dst;
+
+  int disposition = LIBWINPATH_FILE_OPEN;
+  if (flags & O_CREAT)
+    disposition = LIBWINPATH_FILE_CREATE;
+  else if (flags & O_WRONLY) {
+    disposition = LIBWINPATH_FILE_OVERWRITE;
+  }
+
+  if ((errno = -libwinpath_getpath(&dst, pathname, disposition))) {
+    return -1;
+  }
+
+  return open(pathname, flags, mode);
+}
 
 FILE* libwinpath_fopen(const char* filename, const char* mode) {
   if (libwinpath_disable_wrapper)
